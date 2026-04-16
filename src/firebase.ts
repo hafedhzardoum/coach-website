07cfe3c994a31a113778ab0b8129e1af4457c4b0
hafedhzardoum <hafedhzar13@gmail.com>
2026-04-16 02:16:49 +0200

    Refactor Firebase initialization and imports
    Updated Firebase initialization to use config from JSON file.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,9 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import config from "./firebase-applet-config.json";
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+const app = initializeApp(config);
+
+export const db = getFirestore(app);
 export const auth = getAuth(app);
